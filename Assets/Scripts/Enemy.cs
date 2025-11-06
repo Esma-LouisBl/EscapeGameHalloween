27370cd26d4847ffb0a9d27130a1d9ea978f715e
BlondeauLouis <l.blondeau@rns.ecolescreatives.com>
2025-11-06 09:11:31 +0100

    feinte

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,7 +10,7 @@ public class Enemy : MonoBehaviour
 
     public bool hasSeenPlayer = false, lookingAtPlayer = false;
     private int _time;
-    private bool _needingNumber = true, _isWaiting = false, _needToLookPlayer = false;
+    private bool _needingNumber = true, _isWaiting = false, _needToLookPlayer = false, _feint;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,10 +26,21 @@ public class Enemy : MonoBehaviour
             _isWaiting = true;
             StartCoroutine(Wait(_time));
         }
-        else if (_needingNumber && !_isWaiting)
+
+        else if (_needingNumber && !_isWaiting) //génération du temps d'attente et feinte ou non
         {
             _time = Random.Range(10, 41);
             _needingNumber = false;
+
+            int r = Random.Range(0, 2);
+            if (r == 0)
+            {
+                _feint = false;
+            }
+            else
+            {
+                _feint = true;
+            }
         }
 
         if (_needToLookPlayer)
@@ -41,7 +52,14 @@ public class Enemy : MonoBehaviour
 
     private void TurnBack()
     {
-        StartCoroutine(Rotating());
+        if (_feint)
+        {
+            StartCoroutine(FeintRotating());
+        }
+        else
+        {
+            StartCoroutine(Rotating());
+        }
     }
 
     private IEnumerator Rotating()
@@ -74,6 +92,31 @@ public class Enemy : MonoBehaviour
     private IEnumerator Returning()
     {
         for (int i = 0; i < 180; i++)
+        {
+            _enemyTransform.Rotate(0, -1, 0);
+            _light.intensity -= 10;
+            yield return new WaitForSeconds(.01f);
+        }
+        _needingNumber = true;
+        _isWaiting = false;
+    }
+
+    private IEnumerator FeintRotating()
+    {
+        for (int i = 0; i < 90; i++)
+        {
+            _enemyTransform.Rotate(0, 1, 0);
+            _light.intensity += 10;
+            yield return new WaitForSeconds(.01f);
+        }
+
+        yield return new WaitForSeconds(1);
+        StartCoroutine(FeintReturning());
+
+    }
+    private IEnumerator FeintReturning()
+    {
+        for (int i = 0; i < 90; i++)
         {
             _enemyTransform.Rotate(0, -1, 0);
             _light.intensity -= 10;
