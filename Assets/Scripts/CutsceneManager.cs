1c2d740ed5513d8d2238e9dcb2d3447beec9c46e
BlondeauLouis <l.blondeau@rns.ecolescreatives.com>
2025-11-13 00:44:11 +0100

    sons intro

--- Assets/Scripts/CutsceneManager.cs
+++ Assets/Scripts/CutsceneManager.cs
@@ -5,10 +5,14 @@ using UnityEngine.SceneManagement;
 public class CutsceneManager : MonoBehaviour
 {
     [SerializeField] private Animator _enemyAnimator;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _clip1, _clip2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _enemyAnimator.SetTrigger("Scenedebut");
+        _audioSource.clip = _clip1;
+        _audioSource.Play();
         StartCoroutine(Intro());
     }
 
@@ -22,6 +26,8 @@ public class CutsceneManager : MonoBehaviour
     {
         yield return new WaitForSeconds(6);
         _enemyAnimator.SetTrigger("Take");
+        _audioSource.clip = _clip2;
+        _audioSource.Play();
         yield return new WaitForSeconds(2);
         SceneManager.LoadSceneAsync(1);
     }
